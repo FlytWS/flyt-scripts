b13faf355d52767bc7f2b7dc9868581416bb4f8a
FlytWS <flytws@outlook.com>
2024-10-18 15:13:14 +0100

    2410181513

--- flyt-usb.py
+++ flyt-usb.py
@@ -1,25 +1,25 @@
-import re
-import subprocess
+#!/usr/bin/env python
+
 import json
+import subprocess
+import re
 
-# Flyt USB
-
-device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
-df = subprocess.check_output("lsusb")
-
-print(df)
 
 devices = []
-for i in df.split(b'\n'):
+device_re = re.compile("Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
+df = subprocess.check_output("lsusb", universal_newlines=True)
+for i in df.split('\n'):
     if i:
-        info = device_re.match(i)
-        if info:
-            dinfo = info.groupdict()
+        _inf = device_re.match(i)
+        if _inf:
+            dinfo = _inf.groupdict()
             dinfo['device'] = '/dev/bus/usb/%s/%s' % (dinfo.pop('bus'), dinfo.pop('device'))
             devices.append(dinfo)
-            
 
-print(devices)
 
-#with open("/etc/flyt/data/flyt-usb.json", "w") as jsonFile:
-#    json.dump(devices, jsonFile)
+
+with open("/etc/flyt/data/flyt-usb-raw.json", 'w+') as file:
+    file.write(df)
+
+with open("/etc/flyt/data/flyt-usb-parse.json", "w") as jsonFile:
+    json.dump(devices, jsonFile)
