cb5648f3408416ce77a92bb6b66f30b4da2de9a7
FlytWS <flytws@outlook.com>
2024-10-18 14:46:23 +0100

    2410181446

--- flyt-usb-parse.py
+++ flyt-usb-parse.py
@@ -21,10 +21,6 @@ import subprocess
 import sys
 
 
-ONE = 1
-MANY = 2
-
-
 
 
 def split_nodes(lines):
@@ -32,13 +28,10 @@ def split_nodes(lines):
     nodes = []
     current_node = lines.pop(0)
     current_subnodes = []
-    # The "END" is just a sentinel so that we don't have to make
-    # a special case for the last entry in the list.
     for line in lines + ["END"]:
         if line.startswith("  "):
             current_subnodes.append(line[2:])
         else:
-            # Hack to work around weird stuff with "HID Device Descriptor:"
             if current_node.startswith("iInterface"):
                 current_subnodes = []
             if current_subnodes:
@@ -58,14 +51,6 @@ def make_tree(nodes):
         elif type(node) == list:
             assert len(node) == 2
             key = node[0]
-
-            if node_arity == ONE:
-                assert key not in tree
-                tree[key] = make_tree(node[1])
-            else:  # arity[key] == MANY:
-                if key not in tree:
-                    tree[key] = []
-                tree[key].append(make_tree(node[1]))
         else:
             raise ValueError("Wrong node type: {}".format(type(node)))
     return tree
@@ -81,11 +66,7 @@ def parse(device):
 
 
 
-
-
-
 devices = []
-# input_file = sys.stdin
 input_file = subprocess.check_output("lsusb").decode()
 for device in input_file.split("\n\n"):
     device = device.strip()
@@ -93,13 +74,7 @@ for device in input_file.split("\n\n"):
     devices.append(tree)
 
 
-if len(sys.argv) == 1:
-    json.dump(devices, sys.stdout)
-    exit(0)
-
-
 reports = []
-
 for device in devices:
     descriptor = device["Device Descriptor"]
     interfaces = descriptor["Configuration Descriptor"][0]["Interface Descriptor"]
@@ -121,8 +96,7 @@ for device in devices:
 for report in reports:
     basename = "devicereports/{}_{}_USB{}".format(
         report["idVendor"].split()[0],
-        report["idProduct"].split()[0],
-        report["bcdUSB"],
+        report["idProduct"].split()[0]
     )
 
 print("## REPORT ")
