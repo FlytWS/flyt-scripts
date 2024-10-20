3f311f3ba289bcf8714a2a5be4ed638fed3a1eaf
FlytWS <flytws@outlook.com>
2024-10-20 22:05:47 +0100

    2410202205

--- flyt-bandwidth.py
+++ flyt-bandwidth.py
@@ -3,26 +3,14 @@ import subprocess
 
 # Flyt Bandwidth
 
+status = ""
 
-#data = {}
-
-output = {}
-
-
-# vnstat
 try:
     output = subprocess.run(["vnstat", "--json"], capture_output=True)
-    #status = output.stdout.decode().strip()
     status = output.stdout.decode()
-    #data['vnstat'] = status
-	#data["vnstat"] = stat
 except:
 	print("Error "+str(IOError))
 	pass
 
-
-#with open("/etc/flyt/data/flyt-bandwidth.json", "w") as jsonFile:
-#    json.dump(data, jsonFile)
-    
 with open("/etc/flyt/data/flyt-bandwidth.json", "w") as file:
     file.write(status)
