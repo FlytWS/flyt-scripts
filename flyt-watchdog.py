0782f00f5bba36f7d33c9f0a001cb3a62ed9a27a
FlytWS <flytws@outlook.com>
2024-11-11 13:04:43 +0000

    2411111304

--- flyt-watchdog.py
+++ flyt-watchdog.py
@@ -14,9 +14,9 @@ def boot():
     subprocess.call(["python3", "/etc/flyt/scripts/flyt-wifi-scan.py"])
     subprocess.call(["python3", "/etc/flyt/scripts/flyt-wifi-manage.py"])
     subprocess.call(["python3", "/etc/flyt/scripts/flyt-wifi-active.py"])    
-    #subprocess.call(["python3", "/etc/flyt/scripts/flyt-request.py"])  
+    subprocess.Popen(["python3", "/etc/flyt/scripts/flyt-request.py"])  
     subprocess.call(["bash", "/etc/flyt/scripts/register-node.sh"])
-    #subprocess.call(["python3", "/etc/flyt/scripts/flyt-watchdog-network.py"])
+    subprocess.Popen(["python3", "/etc/flyt/scripts/flyt-watchdog-network.py"])
     subprocess.call(["python3", "/etc/flyt/scripts/flyt-ecc-key.py"])
     subprocess.call(["python3", "/etc/flyt/scripts/flyt-angry-purple-tiger.py"])    
 
